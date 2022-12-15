4d870b84aff80bb88a13eb11ca01edf113b5ec55
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-12-15 13:43:45 +0300

    assets and css folder created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './index.css';
+import './css/index.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="">
-      
+    <div className="relative">
+      <img className='object-cover h-40 w-80' src="" alt="logo" />
+      <Navbar />
     </div>
   );
 }
