cf07961ce2a61765dd6ce6829fed6de26c1211c5
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-12-15 13:43:58 +0300

    navbar edited

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav>
+      {/* <ul>
+        <li className="flex items-end">
+          <h1 className="text-2xl">
+            Ali Kerem Ata's
+          </h1>
+          <p>Portfolio</p>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul> */}
+    </nav>
+  )
+}
