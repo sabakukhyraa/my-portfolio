a1d0cf4b0df08c4533b127f5a42d74c74c97674d
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-12-17 15:14:30 +0300

    background fixed again

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Contact from './views/Contact'
 function App() {
 
   return (
-    <div className="relative bg-back overflow-hidden background-css">
+    <div className="relative bg-back overflow-hidden">
       <a href="https://www.linkedin.com/in/ali-kerem-ata-11149521a/" className='fixed top-4 right-4 lg:top-8 lg:right-8 object-cover px-6 py-2 rounded-full text-2xl lg:text-4xl font-medium hover:scale-105 transition-all duration-300 ease-in-out hover:text-gray bg-back-fronter text-semigray backdrop-blur bg-opacity-60 z-[999]'>Ali Kerem Ata</a>
       <Router>
         <Navbar />
--- src/views/Home.js
+++ src/views/Home.js
@@ -32,61 +32,56 @@ export default function Home() {
           />
           <div className="flex flex-col place-content-between">
             <p className="text-lg text-gray">{skill.thoughts}</p>
-            {/* <div className="relative z-30 self-end mt-4 w-24 h-3 lg:w-32 lg:h-6 rounded-full bg-back">
-              <div
-                style={{
-                  backgroundColor: skill.techBaseColor,
-                  width: skill.skillLevel * 100 + "%",
-                }}
-                className="absolute h-full rounded-full"
-              ></div>
-            </div> */}
           </div>
         </div>
       </li>
     ));
 
   return (
-    <div className="flex flex-col container">
-      <div
-        className="flex flex-col items-center justify-center gap-8 h-[100vh] "
-        data-aos="zoom-in"
-      >
-        <img
-          className="object-contain w-40 h-40 rounded-full shadow-lg aspect-square"
-          src="/profile.png"
-          alt="profile"
-        />
-        <h1 className="text-5xl font-bold text-gray">Ali Kerem Ata</h1>
-        <div className="text-center">
-          <p className="text-xl font-semibold text-gray">
-            Hi! You can find my personal information, projects and career
-            history in this website.
-          </p>
-          <p className="text-xl font-semibold text-gray">
-            If you desire to work with me please contact me.
-          </p>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center gap-8 pb-24 ">
-        <h1 className="self-center mb-8 text-5xl font-bold text-gray">
-          Techs I Use:
-        </h1>
-        <div className="mb-24 skills">
-          <h2 className="font-light text-7xl text-semigray">Frontend</h2>
-          <p className="text-lg font-medium text-gray">
-            I'm working as a Frontend Developer for 1.5 years. I think writing
-            Frontend is perfect for reducing stress!
-          </p>
-          <ul>{skills(techsIUse[0])}</ul>
-        </div>
-        <div className="skills">
-          <h2 className="font-light text-7xl text-semigray">Backend</h2>
-          <p className="text-lg font-medium text-gray">
-            I'm pretty new at Backend. I'm trying to learn everything and while
-            I learn Backend, I understand Frontend better.
-          </p>
-          <ul>{skills(techsIUse[1])}</ul>
+    <div className="background-css">
+      <div className="container">
+        <div className="flex flex-col">
+          <div
+            className="flex flex-col items-center justify-center gap-8 h-[100vh] "
+            data-aos="zoom-in"
+          >
+            <img
+              className="object-contain w-40 h-40 rounded-full shadow-lg aspect-square"
+              src="/profile.png"
+              alt="profile"
+            />
+            <h1 className="text-5xl font-bold text-gray">Ali Kerem Ata</h1>
+            <div className="text-center">
+              <p className="text-xl font-semibold text-gray">
+                Hi! You can find my personal information, projects and career
+                history in this website.
+              </p>
+              <p className="text-xl font-semibold text-gray">
+                If you desire to work with me please contact me.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-8 pb-24">
+            <h1 className="self-center mb-8 text-5xl font-bold text-gray">
+              Techs I Use:
+            </h1>
+            <div className="mb-24 skills">
+              <h2 className="font-light text-7xl text-semigray">Frontend</h2>
+              <p className="text-lg font-medium text-gray">
+                I'm working as a Frontend Developer for 1.5 years. I think
+                writing Frontend is perfect for reducing stress!
+              </p>
+              <ul>{skills(techsIUse[0])}</ul>
+            </div>
+            <div className="skills">
+              <h2 className="font-light text-7xl text-semigray">Backend</h2>
+              <p className="text-lg font-medium text-gray">
+                I'm pretty new at Backend. I'm trying to learn everything and
+                while I learn Backend, I understand Frontend better.
+              </p>
+              <ul>{skills(techsIUse[1])}</ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
