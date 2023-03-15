011c364a8b9a733e94311f7a6a0cede20ffb63e7
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2023-03-15 17:34:12 +0300

    projects screen fixed

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -39,36 +39,34 @@ export default function Projects() {
 
   return (
     <div className="background-css">
-      <div className="container">
-        <div className="flex flex-col items-center justify-center min-h-screen gap-12 py-12 lg:px-24 lg:py-3">
-          <h1 className="pt-6 text-2xl font-light text-center text-gray">
-            Waiting for permission to share the projects I worked on...
-            (click the projects to see them)
-          </h1>
-          <Swiper
-            breakpoints={{
-              968: {
-                slidesPerView: 2.5,
-              },
-            }}
-            effect={"coverflow"}
-            centeredSlides={true}
-            slidesPerView={1}
-            spaceBetween={4}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 270,
-              modifier: 1,
-              slideShadows: false,
-            }}
-            pagination={pagination}
-            modules={[EffectCoverflow, Pagination]}
-            className="flex flex-col items-center justify-center w-full gap-2 p-2 lg:gap-12 lg:p-12"
-          >
-            {projectSlide}
-          </Swiper>
-        </div>
+      <div className="flex flex-col items-center justify-center min-h-screen gap-12 px-12 py-12 lg:px-32 lg:py-3">
+        <h1 className="pt-6 text-2xl font-light text-center text-gray">
+          Waiting for permission to share the projects I worked on... (click the
+          projects to see them)
+        </h1>
+        <Swiper
+          breakpoints={{
+            968: {
+              slidesPerView: 2.5,
+            },
+          }}
+          effect={"coverflow"}
+          centeredSlides={true}
+          slidesPerView={1}
+          spaceBetween={4}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 270,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={pagination}
+          modules={[EffectCoverflow, Pagination]}
+          className="flex flex-col items-center justify-center w-full gap-2 p-2 lg:gap-12 lg:p-12"
+        >
+          {projectSlide}
+        </Swiper>
       </div>
     </div>
   );
