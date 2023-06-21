e7291cbaf2c06f16a35fb089b5cdaf1290368cda
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2023-06-21 19:41:36 +0300

    Resume updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="relative overflow-hidden bg-back">
       <a
-        href="/February_Kerem_Ata.pdf"
+        href="/JulyResume2023.pdf"
         target="_blank"
         className="fixed top-4 right-4 lg:top-8 lg:right-8 object-cover px-6 py-2 rounded-full text-2xl lg:text-4xl font-medium hover:scale-105 transition-all duration-300 ease-in-out hover:text-gray bg-back-fronter text-semigray backdrop-blur bg-opacity-60 z-[999]"
         rel="noreferrer"
